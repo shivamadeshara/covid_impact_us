c307ecc43fe250d1c967a350bee2eafc399fa0cd
bansalvishal1184 <46757929+bansalvishal1184@users.noreply.github.com>
2020-07-19 17:42:17 -0700

    Add files via upload

---
+++ read_file.py
@@ -0,0 +1,9 @@
+# If you haven't installed pip --> install via Homebrew(brew install pip)
+# Installation Command: run it in terminal(pip install pandas)
+import pandas as pd
+
+# Replace the given path with that of whichever file you want to read in
+file = pd.read_csv("/Users/vishalbansal/Downloads/Shutdown-Reopen Dates & Parameters.csv")
+
+# Place functions to perform on "file" here
+
